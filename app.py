6e03e208a0bfa48eeb7d0a981063c2e60602d117
Adrian Zech <zech.adrian.at@gmail.com>
2019-01-23 20:10:16 +0100

    Added backup function

--- app.py
+++ app.py
@@ -16,6 +16,27 @@ folders = [
 option = input("Enter number: ")
 
 
+def backup():
+    def zipdir(path, ziph):
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                ziph.write(os.path.join(root, file),
+                           os.path.relpath(os.path.join(root, file),
+                                           os.path.join(path, "..")))
+
+    def zipit(dir_list, zip_name):
+        zipf = zipfile.ZipFile(zip_name, "w", zipfile.ZIP_DEFLATED)
+        for dir in dir_list:
+            zipdir(dir, zipf)
+        zipf.close()
+
+    try:
+        zipit(folders, filename)
+        print("Created archive")
+    except:
+        print("Failed to create archive.")
+
+
 def zip():
     def zipdir(path, ziph):
         for root, dirs, files in os.walk(path):
