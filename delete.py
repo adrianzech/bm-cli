b8336a04a763c400891b29572152dd100372c1aa
Adrian Zech <zech.adrian.at@gmail.com>
2019-01-23 19:27:04 +0100

    Added ftp delete function

---
+++ delete.py
@@ -0,0 +1,25 @@
+from ftplib import FTP
+
+host = "159.69.90.146"
+username = "kbm"
+password = "underwolf99912"
+
+path = "/"
+filename = "backup.zip"
+
+ftp = FTP(host)
+print(ftp.getwelcome())
+
+try:
+    ftp.login(username, password)
+    print("Logged in.")
+except:
+    print("Failed to login.")
+
+try:
+    ftp.cwd(path)
+    ftp.delete(filename)
+    ftp.quit()
+    print("Deleted file.")
+except:
+    print("Failed to delete file")
