b2c986184836acad6599967cd703119e2c7513a9
Daniel Siebert <daniel.siebert@protonmail.com>
2019-01-23 19:49:43 +0100

    Test

--- zip.py
+++ zip.py
@@ -6,13 +6,6 @@ folders = [
     "data/addons",
     "data/userdata"]
 
-"""
-def zipdir(path, ziph):
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            ziph.write(os.path.join(root, file))
-"""
-
 
 def zipdir(path, ziph):
     for root, dirs, files in os.walk(path):
@@ -29,18 +22,10 @@ def zipit(dir_list, zip_name):
     zipf.close()
 
 
-""""
 try:
-    zipf = zipfile.ZipFile(filename, "w", zipfile.ZIP_DEFLATED)
-    zipdir("data/", zipf)
-    zipf.close()
     zipit(folders, filename)
     print("Created archive")
 except:
     print("Failed to create archive.")
-"""
-
-zipit(folders, filename)
-print("Created archive")
 
 # https://stackoverflow.com/questions/46229764/python-zip-multiple-directories-into-one-zip-file/46267469
